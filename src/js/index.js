54ab608abd32d72738f16f29b876baa60649c0d1
Juan Tobias <juantobiasdl7@gmail.com>
2020-05-08 01:04:06 -0500

    I added a load image to display while we wait for the API response

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,38 @@
 import Search from './models/Search';
+import * as searchView from './View/SearchView';
+import { elements, renderLoader, clearLoader } from './View/base';
 
-const state = {
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shoping list object
+ * - Liked recipes
+ */
+const state = {};
 
-};
+const controlSearch = async () => {
+    // 1) get query from view
+    const query = searchView.getInput();
 
-const search = new Search('pizza');
-console.log(search);
+    if(query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
 
-search.getResults();
+        // 3) Prepare UI for results 
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
